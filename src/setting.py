ef4095dcf7a941ddc85fd6b0ed4d3660b9d21c77
tasuren <tasuren_0618@yahoo.co.jp>
2022-01-22 13:57:27 +0900

    coding: ダッシュボードのコマンドのデータの型の更新をした。

--- src/setting.py
+++ src/setting.py
@@ -1,9 +1,12 @@
 # Setting, Description: ダッシュボードのコマンドのデータの型等がある。
 
-from typing import TypedDict
+from __future__ import annotations
+
+from typing import TypedDict, Union, Literal
 
 
 class CommandData(TypedDict, total=False):
-    headding: str
-    category: str
-    help: str
+    headding: dict[str, str]
+    category: dict[str, str]
+    help: dict[str, str]
+    kwargs: dict[str, Union[Literal["Chanenl", "Role", "User"], str]]
