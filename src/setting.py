b7094fae453f0cbd93ae33b46cc7dea3e59134fc
tasuren <tasuren_0618@yahoo.co.jp>
2022-01-23 19:15:27 +0900

    update: kwargsを型に設定した。

--- src/setting.py
+++ src/setting.py
@@ -2,11 +2,17 @@
 
 from __future__ import annotations
 
-from typing import TypedDict, Union, Literal
+from typing import TypedDict, Union, Optional, Literal, Any
+
+
+class Kwargs(TypedDict):
+    type: Union[Literal["Chanenl", "Role", "User", "Literal", "str", "int", "float"], str]
+    default: Union[str, int, float, None]
+    extra: Optional[Union[dict, Any]]
 
 
 class CommandData(TypedDict, total=False):
     headding: dict[str, str]
     category: dict[str, str]
     help: dict[str, str]
-    kwargs: dict[str, Union[Literal["Chanenl", "Role", "User"], str]]
+    kwargs: dict[str, Kwargs]
